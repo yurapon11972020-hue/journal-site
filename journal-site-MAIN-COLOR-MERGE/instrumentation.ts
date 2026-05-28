079c77037a7880c70d56e84178ef16339091d443
1 <yurapon11972020@gmail.com>
2026-05-28 15:01:55 +0500

    Delete journal-site-MAIN-COLOR-MERGE directory

--- journal-site-MAIN-COLOR-MERGE/instrumentation.ts
+++
@@ -1,8 +0,0 @@
-export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
-    const { startJournalCache } = await import('./lib/yandex-disk');
-    void startJournalCache().catch((error) => {
-      console.error('[journal-cache] Стартовая загрузка журнала не удалась:', error);
-    });
-  }
-}
